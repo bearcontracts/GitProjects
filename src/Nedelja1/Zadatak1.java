298455c20985bf698f737c7dd00e1fbe97d216f8
Nikola <djuricicnikola@outlook.com>
2022-08-15 20:02:23 +0200

    Izmena zadataka

--- src/Nedelja1/Zadatak1.java
+++ src/Nedelja1/Zadatak1.java
@@ -1,7 +1,22 @@
 package Nedelja1;
 
+import java.util.Scanner;
+
 public class Zadatak1 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner s =new Scanner(System.in);
+        int[][]arr = new int[3][3];
+        int suma =0;
+        for(int i =0; i<arr.length;i++){
+            for(int j =0; j<arr[i].length;j++){
+                arr[i][j]=s.nextInt();
+            }
+        }
+        for(int i =0; i<arr.length;i++){
+            for(int j =0; j<arr[i].length;j++){
+                suma +=arr[i][j];
+            }
+        }
+        System.out.println("Suma je " +suma);
     }
 }
--- src/Nedelja2/Zadatak1.java
+++ src/Nedelja2/Zadatak1.java
@@ -1,7 +1,29 @@
 package Nedelja2;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Zadatak1 {
+    public static String reverseWord(String str){
+        if(str.equals("")){
+            return str;
+        }
+        Stack<Character> reverse = new Stack<>();
+        char [] c= str.toCharArray();
+        for(int i = 0;i<str.length();i++){
+            reverse.push(c[i]);
+        }
+
+        int j = 0;
+        while(!reverse.isEmpty()){
+            c[j++]=reverse.pop();
+        }
+        return String.copyValueOf(c);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hellow");
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        System.out.println(reverseWord(str));
     }
 }
